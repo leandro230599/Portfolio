8deb805dbdc78205762d11f2ad7c8ccc74a2b7e9
Leandro Lopez <leandro230599@outlook.com>
2023-01-13 20:16:09 -0300

    Aplique particles.js al background

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useCallback } from 'react'
 import './App.css'
+import Particles from 'react-tsparticles'
+import {loadFull} from 'tsparticles'
+import particlesConfig from '../config/particles-config.json'
 
 function App() {
   const [count, setCount] = useState(0)
+  const particlesInit = useCallback(main => {
+    loadFull(main);
+  }, [])
 
   return (
     <div className="App">
+      <Particles options={particlesConfig} init={particlesInit}/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
